ab08263bfa72fd2c71b6e6326abdfaf07c1a9b17
dylan <dylan@dylanreed.com>
2022-02-07 11:06:58 -0700

    Added assets

--- giffyboy.py
+++ giffyboy.py
@@ -8,6 +8,6 @@ for file_name in sorted(os.listdir(png_dir)):
     if file_name.endswith('.png'):
         file_path = os.path.join(png_dir, file_name)
         images.append(imageio.imread(file_path))
-imageio.mimsave('gif/movie.gif', images)
+imageio.mimsave('gif/movie.gif', images, fps = 6)
 
 optimize('gif/movie.gif')
--- twister.py
+++ twister.py
@@ -8,161 +8,92 @@ import json
 # Each image is made up a series of traits
 # The weightings for each trait drive the rarity and add up to 100%
 
-#background = ["balloon", "basket case", "black and blue", "nervous", "orange is the new green", "pretty", "too bright"]  
-#background_weights =  [15, 15, 14, 14, 14, 14, 14]
 
-body = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"] 
-body_weights = [14, 15, 15, 14, 14, 14, 14]
+backgrounds = ["BLACK", "GREY", "GREEN", "MARION", "YELLOW", "MAGENTA", "CONFETTI MIST", "NEWSPRINT", "PINK", "BLUE", "PURPLE CRACK", "3D BLUE"] 
+backgrounds_weights = [0.12, 0.12, 0.12, 0.12, 0.12, 0.07, 0.07, 0.07, 0.07, 0.055, 0.055, 0.01]
 
-leftbackleg = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"] 
-leftbackleg_weights = [14, 14, 15, 15, 14, 14, 14]
+bodies = ["BLUEBERRY", "KELLY", "PURPLE", "RED", "BROWN", "GREY", "SKETCH"] 
+bodies_weights = [0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.1]
 
-leftear = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"] 
-leftear_weights = [14, 14, 14, 15, 15, 14, 14]
+wears = ["NAKED", "POWER TIE", "PARTY TIE", "NEWAVE SKINNY TIE", "CLOWN BOW TIE", "CHIPPY", "BLACK TIE", "PICNIC TIE", "CIRCUS SCARF", "BUDDY BOW TIE", "DANCING"] 
+wears_weights = [0.35, 0.08, 0.08, 0.08, 0.08, 0.08, 0.08, 0.05, 0.05, 0.05, 0.02]
 
-leftfrontleg = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"] 
-leftfrontleg_weights = [14, 14, 14, 14, 15, 15, 14]
+moods = ["AHHH", "BLBL", "BLSS", "DOHH", "GRRR", "MMMM", "OHHH", "UHHH", "WHOA", "ZZZZ", "BLAH", "GLAH", "HMPH", "NRVS"] 
+moods_weights = [0.07142857143, 0.07142857143, 0.07142857143, 0.07142857143, 0.07142857143, 0.07142857143, 0.07142857143, 0.07142857143, 0.07142857143, 0.07142857143, 0.07142857143, 0.07142857143, 0.07142857143, 0.07142857143]
 
-neck = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"] 
-neck_weights = [14, 14, 14, 14, 14, 15, 15]
+atmos = ["CLEAR", "BUBBLES", "CONFETTI", "HONEY", "RAIN"] 
+atmos_weights = [0.6, 0.1, 0.1, 0.1, 0.1]
 
-nose = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"] 
-nose_weights = [15, 14, 14, 14, 14, 14, 15]
-
-rightbackleg = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"] 
-rightbackleg_weights = [15, 15, 14, 14, 14, 14, 14]
-
-rightear = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"] 
-rightear_weights = [14, 15, 15, 14, 14, 14, 14]
-
-rightfrontleg = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]  
-rightfrontleg_weights = [14, 14, 15, 15, 14, 14, 14]
-
-tail = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]  
-tail_weights = [14, 14, 14, 15, 15, 14, 14]
-
-outline = ["black"]  
-outline_weights = [100]
 
 # Dictionary variable for each trait. 
 # Eech trait corresponds to its file name
 # Add more shapes and colours as you wish
 
-#background_files = {
-#    "balloon": "balloon",
-#    "basket case": "basket-case",
-#    "black and blue": "black-blue",
-#    "nervous": "nervous",
-#    "orange is the new green": "orange-green",
-#    "pretty": "pretty",
-#    "too bright": "too-bright"
-#}
 
-body_files = {
-    "red": "red",
-    "orange": "orange",
-    "yellow": "yellow",
-    "green": "green",
-    "blue": "blue",
-    "indigo": "indigo",
-    "violet": "violet"
+backgrounds_files = {
+    "BLACK": "09MB_NFT-BKG-BLK",
+    "GREY": "09MB_NFT-BKG-GREY",
+    "GREEN": "09MB_NFT-BKG-MNTG",
+    "MARION": "09MB_NFT-BKG-MRNG",
+    "YELLOW": "09MB_NFT-BKG-YLWG",
+    "MAGENTA": "09MB_NFT-BKG-MGNT",
+    "CONFETTI MIST": "09MB_NFT-BKG-CNFTG",
+    "NEWSPRINT": "09MB_NFT-BKG-NWSP",
+    "PINK": "09MB_NFT-BKG-PINK",
+    "BLUE": "09MB_NFT-BKG-PRCH",
+    "PURPLE CRACK": "09MB_NFT-BKG-BKBL",
+    "3D BLUE": "01MB_NFT-BKG-3DBLU"
 }
 
-leftbackleg_files = {
-    "red": "red",
-    "orange": "orange",
-    "yellow": "yellow",
-    "green": "green",
-    "blue": "blue",
-    "indigo": "indigo",
-    "violet": "violet"
+bodies_files = {
+    "BLUEBERRY": "15MB_NFT-BLUE_BEAR",
+    "KELLY": "15MB_NFT-GREEN_BEAR",
+    "PURPLE": "15MB_NFT-PRPL_BEAR",
+    "RED": "15MB_NFT-RED_BEAR",
+    "BROWN": "15MB_NFT-BRWN_BEAR",
+    "GREY": "15MB_NFT-GREY_BEAR",
+    "SKETCH": "10MB_NFT-SKTCH_BEAR"
 }
 
-leftear_files = {
-    "red": "red",
-    "orange": "orange",
-    "yellow": "yellow",
-    "green": "green",
-    "blue": "blue",
-    "indigo": "indigo",
-    "violet": "violet"
+wears_files = {
+    "NAKED": "35MB_NFT-PFP_NAKED",
+    "POWER TIE": "08MB_NFT-PFP_POWER",
+    "PARTY TIE": "08MB_NFT-PFP_PARTY",
+    "NEWAVE SKINNY TIE": "08MB_NFT-PFP_NEWAV",
+    "CLOWN BOW TIE": "08MB_NFT-PFP_CLOWN",
+    "CHIPPY": "08MB_NFT-PFP_CHPNDL",
+    "BLACK TIE": "08MB_NFT-PFP_BLKTIE",
+    "PICNIC TIE": "05MB_NFT-PFP_PICNIC",
+    "CIRCUS SCARF": "05MB_NFT-PFP_LADY",
+    "BUDDY BOW TIE": "05MB_NFT-PFP_BUDDY",
+    "DANCING": "02MB_NFT-PFP_DANCE"
 }
 
-leftfrontleg_files = {
-    "red": "red",
-    "orange": "orange",
-    "yellow": "yellow",
-    "green": "green",
-    "blue": "blue",
-    "indigo": "indigo",
-    "violet": "violet"
+moods_files = {
+    "AHHH": "MB_NFT-PFP_AHHH",
+    "BLBL": "MB_NFT-PFP_BLBL",
+    "BLSS": "MB_NFT-PFP_BLSS",
+    "DOHH": "MB_NFT-PFP_DOHH",
+    "GRRR": "MB_NFT-PFP_GRRR",
+    "MMMM": "MB_NFT-PFP_MMMM",
+    "OHHH": "MB_NFT-PFP_OHHH",
+    "UHHH": "MB_NFT-PFP_UHHH",
+    "WHOA": "MB_NFT-PFP_WHOA",
+    "ZZZZ": "MB_NFT-PFP_ZZZZ",
+    "BLAH": "MB_NFT-PFP_BLAH",
+    "GLAH": "MB_NFT-PFP_GLAH",
+    "HMPH": "MB_NFT-PFP_HMPH",
+    "NRVS": "MB_NFT-PFP_NRVS"
 }
 
-neck_files = {
-    "red": "red",
-    "orange": "orange",
-    "yellow": "yellow",
-    "green": "green",
-    "blue": "blue",
-    "indigo": "indigo",
-    "violet": "violet"
+atmos_files = {
+    "CLEAR": "60MB_NFT-PFP_CLEAR",
+    "BUBBLES": "10MB_NFT-PFP_BUBBLES",
+    "CONFETTI": "10MB_NFT-PFP_CNFT",
+    "HONEY": "10MB_NFT-PFP_HONEY",
+    "RAIN": "10MB_NFT-PFP_RAIN"
 }
 
-nose_files = {
-    "red": "red",
-    "orange": "orange",
-    "yellow": "yellow",
-    "green": "green",
-    "blue": "blue",
-    "indigo": "indigo",
-    "violet": "violet"
-}
-
-outline_files = {
-    "black": "black",
-    #"grey": "grey",
-    "white": "white"
-}
-
-rightbackleg_files = {
-    "red": "red",
-    "orange": "orange",
-    "yellow": "yellow",
-    "green": "green",
-    "blue": "blue",
-    "indigo": "indigo",
-    "violet": "violet"
-}
-
-rightear_files = {
-    "red": "red",
-    "orange": "orange",
-    "yellow": "yellow",
-    "green": "green",
-    "blue": "blue",
-    "indigo": "indigo",
-    "violet": "violet"
-}
-
-rightfrontleg_files = {
-    "red": "red",
-    "orange": "orange",
-    "yellow": "yellow",
-    "green": "green",
-    "blue": "blue",
-    "indigo": "indigo",
-    "violet": "violet"
-}
-
-tail_files = {
-    "red": "red",
-    "orange": "orange",
-    "yellow": "yellow",
-    "green": "green",
-    "blue": "blue",
-    "indigo": "indigo",
-    "violet": "violet"
-}
 #Create a function to generate unique image combinations
 TOTAL_IMAGES = 100 # Number of random unique images we want to generate ( 2 x 2 x 2 = 8)
 
@@ -174,18 +105,11 @@ def create_new_image():
 
     # For each trait category, select a random trait based on the weightings 
     #new_image ["Background"] = random.choices(background, background_weights)[0]
-    new_image ["Body"] = random.choices(body, body_weights)[0]
-    new_image ["Left Back Leg"] = random.choices(leftbackleg, leftbackleg_weights)[0]
-    new_image ["Left Ear"] = random.choices(leftear, leftear_weights)[0]
-    new_image ["Left Front Leg"] = random.choices(leftfrontleg, leftfrontleg_weights)[0]
-    new_image ["Neck"] = random.choices(neck, neck_weights)[0]
-    new_image ["Nose"] = random.choices(nose, nose_weights)[0]
-    new_image ["Outline"] = random.choices(outline, outline_weights)[0]
-    new_image ["Right Back Leg"] = random.choices(rightbackleg, rightbackleg_weights)[0]
-    new_image ["Right Ear"] = random.choices(rightear, rightear_weights)[0]
-    new_image ["Right Front Leg"] = random.choices(rightfrontleg, rightfrontleg_weights)[0]
-    new_image ["Tail"] = random.choices(tail, tail_weights)[0]
-
+    new_image ["backgrounds"] = random.choices(backgrounds, backgrounds_weights)[0]
+    new_image ["bodies"] = random.choices(bodies, bodies_weights)[0]
+    new_image ["wears"] = random.choices(wears, wears_weights)[0]
+    new_image ["moods"] = random.choices(moods, moods_weights)[0]
+    new_image ["atmos"] = random.choices(atmos, atmos_weights)[0]
 
     if new_image in all_images:
         return create_new_image()
@@ -221,81 +145,42 @@ print(all_images)
 
 #get trait count
 
-#background_count = {}
-#for item in background:
-#    background_count[item] = 0
+backgrounds_count = {}
+for item in backgrounds:
+    backgrounds_count[item] = 0
 
-body_count = {}
-for item in body:
-    body_count[item] = 0
+bodies_count = {}
+for item in bodies:
+    bodies_count[item] = 0
 
-leftbackleg_count = {}
-for item in leftbackleg:
-    leftbackleg_count[item] = 0
+wears_count = {}
+for item in wears:
+    wears_count[item] = 0
 
-leftear_count = {}
-for item in leftear:
-    leftear_count[item] = 0
+moods_count = {}
+for item in moods:
+    moods_count[item] = 0
 
-leftfrontleg_count = {}
-for item in leftfrontleg:
-    leftfrontleg_count[item] = 0
+atmos_count = {}
+for item in atmos:
+    atmos_count[item] = 0
 
-neck_count = {}
-for item in neck:
-    neck_count[item] = 0
-
-nose_count = {}
-for item in nose:
-    nose_count[item] = 0
-
-outline_count = {}
-for item in outline:
-    outline_count[item] = 0
-
-rightbackleg_count = {}
-for item in rightbackleg:
-    rightbackleg_count[item] = 0
-
-rightear_count = {}
-for item in rightear:
-    rightear_count[item] = 0
-
-rightfrontleg_count = {}
-for item in rightfrontleg:
-    rightfrontleg_count[item] = 0
-
-tail_count = {}
-for item in tail:
-    tail_count[item] = 0
 
 for image in all_images:
     #background_count[image["Background"]] += 1
-    body_count[image["Body"]] += 1
-    leftbackleg_count[image["Left Back Leg"]] += 1
-    leftear_count[image["Left Ear"]] += 1
-    leftfrontleg_count[image["Left Front Leg"]] += 1
-    neck_count[image["Neck"]] += 1
-    nose_count[image["Nose"]] += 1
-    outline_count[image["Outline"]] += 1
-    rightbackleg_count[image["Right Back Leg"]] += 1
-    rightear_count[image["Right Ear"]] += 1
-    rightfrontleg_count[image["Right Front Leg"]] += 1
-    tail_count[image["Tail"]] += 1
+    backgrounds_count[image["backgrounds"]] += 1
+    bodies_count[image["bodies"]] += 1
+    wears_count[image["wears"]] += 1
+    moods_count[image["moods"]] += 1
+    atmos_count[image["atmos"]] += 1
 
 
 #print(background_count)
-print(body_count)
-print(leftbackleg_count)
-print(leftear_count)
-print(leftfrontleg_count)
-print(neck_count)
-print(nose_count)
-print(outline_count)
-print(rightbackleg_count)
-print(rightear_count)
-print(rightfrontleg_count)
-print(tail_count)
+print(backgrounds_count)
+print(bodies_count)
+print(wears_count)
+print(moods_count)
+print(atmos_count)
 
 
 #Generate Metadata for all Traits
@@ -309,33 +194,20 @@ with open(METADATA_FILE_NAME, 'w') as outfile:
 
 for item in all_images:
 
-    #im1 = Image.open(f'./layers/background/{background_files[item["Background"]]}.png').convert('RGBA')
-    im2 = Image.open(f'./layers/body/{body_files[item["Body"]]}.png').convert('RGBA')
-    im3 = Image.open(f'./layers/left back leg/{leftbackleg_files[item["Left Back Leg"]]}.png').convert('RGBA')
-    im4 = Image.open(f'./layers/left ear/{leftear_files[item["Left Ear"]]}.png').convert('RGBA')
-    im5 = Image.open(f'./layers/left front leg/{leftfrontleg_files[item["Left Front Leg"]]}.png').convert('RGBA')
-    im6 = Image.open(f'./layers/neck/{neck_files[item["Neck"]]}.png').convert('RGBA')
-    im7 = Image.open(f'./layers/nose/{nose_files[item["Nose"]]}.png').convert('RGBA')
-    im8 = Image.open(f'./layers/right back leg/{tail_files[item["Tail"]]}.png').convert('RGBA')
-    im9 = Image.open(f'./layers/right ear/{rightbackleg_files[item["Right Back Leg"]]}.png').convert('RGBA')
-    im10 = Image.open(f'./layers/right front leg/{rightear_files[item["Right Ear"]]}.png').convert('RGBA')
-    im11 = Image.open(f'./layers/tail/{rightfrontleg_files[item["Right Front Leg"]]}.png').convert('RGBA')
-    im12 = Image.open(f'./layers/outline/{outline_files[item["Outline"]]}.png').convert('RGBA')
-
+    im1 = Image.open(f'./layers/backgrounds/{backgrounds_files[item["backgrounds"]]}.png').convert('RGBA')
+    im2 = Image.open(f'./layers/bodies/{bodies_files[item["bodies"]]}.png').convert('RGBA')
+    im3 = Image.open(f'./layers/wears/{wears_files[item["wears"]]}.png').convert('RGBA')
+    im4 = Image.open(f'./layers/moods/{moods_files[item["moods"]]}.png').convert('RGBA')
+    im5 = Image.open(f'./layers/atmos/{atmos_files[item["atmos"]]}.png').convert('RGBA')
+    
     #Create each composite
-    com1 = Image.alpha_composite(im2, im3)
-    com2 = Image.alpha_composite(com1, im4)
-    com3 = Image.alpha_composite(com2, im5)
-    com4 = Image.alpha_composite(com3, im6)
-    com5 = Image.alpha_composite(com4, im7)
-    com6 = Image.alpha_composite(com5, im8)
-    com7 = Image.alpha_composite(com6, im9)
-    com8 = Image.alpha_composite(com7, im10)
-    com9 = Image.alpha_composite(com8, im11)
-    com10 = Image.alpha_composite(com9, im12)
-    #com11 = Image.alpha_composite(com10, im12)
+    com1 = Image.alpha_composite(im1, im2)
+    com2 = Image.alpha_composite(com1, im3)
+    com3 = Image.alpha_composite(com2, im4)
+    com4 = Image.alpha_composite(com3, im5)
+
     #Convert to RGB
-    rgb_im = com10.convert('RGB')
+    rgb_im = com4.convert('RGB')
     file_name = str(item["tokenId"]) + ".png"
     rgb_im.save("./images/" + file_name)
 
@@ -362,11 +234,11 @@ for item in all_images:
 #    }
 #    token["attributes"].append(getAttribute("Background", i["Background"]))
 #    token["attributes"].append(getAttribute("Back Legs", i["Back Legs"]))
-#    token["attributes"].append(getAttribute("Body", i["Body"]))
+#    token["attributes"].append(getAttribute("backgrounds", i["backgrounds"]))
 #    token["attributes"].append(getAttribute("Ears", i["Ears"]))
 #    token["attributes"].append(getAttribute("Front Legs", i["Front Legs"]))
 #    token["attributes"].append(getAttribute("Head", i["Head"]))
-#    token["attributes"].append(getAttribute("Neck", i["Neck"]))
+#    token["attributes"].append(getAttribute("atmos", i["atmos"]))
 #    token["attributes"].append(getAttribute("Tail", i["Tail"]))
 #    token["attributes"].append(getAttribute("Outline", i["Outline"]))
 
